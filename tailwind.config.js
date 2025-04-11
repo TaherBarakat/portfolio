84eed813843d77fc86c27c716b24259becb21db1
Taher Barakat <taher.barakat12223@gmail.com>
2025-04-12 01:42:01 +0300

    add scrollbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      // test
       // colors: {
       //   primary: { DEFAULT: "#00FFFF" },
       //   //    primary: { DEFAULT: "#78d927" },
@@ -11,6 +12,7 @@ export default {
       //   text: { DEFAULT: "#00FF00", dark: "#FF0000" },
       // },
 
+      //dark
       colors: {
         primary: { DEFAULT: "#0a192f" },
         //    primary: { DEFAULT: "#78d927" },
@@ -18,6 +20,13 @@ export default {
         accent: { DEFAULT: "#d92778" },
         text: { DEFAULT: "#E6F1FF", dark: "#A8B2D1" },
       },
+      // light
+      // colors: {
+      //   primary: { DEFAULT: "#E6F1FF" }, // Light background
+      //   secondary: { DEFAULT: "#d92778" }, // Kept for contrast
+      //   accent: { DEFAULT: "#d92778" }, // Same as secondary
+      //   text: { DEFAULT: "#0a192f", dark: "#334155" }, // Dark text for readability
+      // },
     },
   },
   plugins: [],
