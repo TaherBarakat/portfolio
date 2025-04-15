9d2e63cc4bd9d2293765d54ab073638d8f556cba
Taher Barakat <taher.barakat12223@gmail.com>
2025-04-15 23:30:50 +0300

    fix the hover effect over the LinkComp

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,19 +4,19 @@ const About = () => {
   return (
     <div
       name="about"
-      className=" text-text bg-primary mb-10 flex h-auto min-h-[90vh] w-full  items-center "
+      className=" mb-10 flex h-auto min-h-[90vh] w-full items-center bg-primary  text-text "
     >
       <div className="flex h-full min-h-full w-full flex-col items-center justify-center">
         <div className="grid w-full max-w-[1000px] grid-cols-2 gap-8">
           <div className=" pb-8 pl-4">
-            <p className="border-accent inline border-b-4 text-4xl font-bold">
+            <p className="inline border-b-4 border-accent text-4xl font-bold">
               About
             </p>
           </div>
           <div></div>
         </div>
         <div className="grid w-full max-w-[1000px] gap-8 px-4 sm:grid-cols-2">
-          <div className="text-text text-4xl font-bold sm:text-right">
+          <div className="text-4xl font-bold text-text sm:text-right">
             <p>
               Hi. I'm Taher Barakat, nice to meet you. I help individuals and
               small businesses launching their first website.
@@ -26,7 +26,7 @@ const About = () => {
             <p className="text-text-dark">
               I am passionate about building excellent software that improves
               the working experiences for other teams. currently working as a
-              front-end developer | data analyst at
+              front-end developer | data analyst at{" "}
               <LinkComp href="http://my.syriatel.sy/index.php">
                 Syriatel
               </LinkComp>{" "}
--- src/components/LinkComp.jsx
+++ src/components/LinkComp.jsx
@@ -1,15 +1,15 @@
-import React, { Children } from "react";
+import React from "react";
 import { FaExternalLinkAlt } from "react-icons/fa";
 
 export default function LinkComp({ children, ...props }) {
   return (
     <a
       target="_blank"
-      className="  text-accent hover:text-accent font-semibold duration-500 hover:text-[1.015rem]"
+      className="inline-flex items-center gap-1 font-semibold text-accent duration-300 hover:scale-[1.05] hover:transform hover:text-accent"
       {...props}
     >
-      {" "}
-      {children} <FaExternalLinkAlt size={12} className="inline" />
+      {children}
+      <FaExternalLinkAlt size={12} className="inline" />
     </a>
   );
 }
